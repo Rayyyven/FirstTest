2f0139c290351a92bb03611e10cb81e74a87d31a
Mike Meury <mike.meury@heig-vd.ch>
2017-09-13 14:54:51 +0200

    Second commit

---
+++ src/source.vhd
@@ -0,0 +1,21 @@
+-------------------------------------------------------------------------------
+-- HEIG-VD, Haute Ecole d'Ingenierie et de Gestion du canton de Vaud
+-- Institut REDS, Reconfigurable & Embedded Digital Systems
+--
+-- File         : source.vhd
+-- Description  : 
+--
+-- Author       : Mike Meury
+-- Date         : 13.09.17
+-- Version      : 0.0
+--
+-- Dependencies : 
+--
+--| Modifications |------------------------------------------------------------
+-- Version   Author Date               Description
+-- 0.0       MIM    13.09.17           Creation
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
